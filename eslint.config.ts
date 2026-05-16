e6a142f44b768174827ddd552e1ff7031356f991
Dominic Egginton <dominic.egginton@gmail.com>
2026-05-16 14:37:15 +0100

    chore: fix linting issues and fail on warnings
    - Add tsconfig.eslint.json to include all project files in type-aware linting\n- Update eslint.config.ts to use the new ESLint-specific tsconfig\n- Update lint script in package.json to fail on warnings

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,31 +4,31 @@ import functional from 'eslint-plugin-functional';
 import prettier from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
-    eslint.configs.recommended,
-    ...tseslint.configs.strict,
-    ...tseslint.configs.stylistic,
-    functional.configs.recommended,
-    functional.configs.externalTypeScriptRecommended,
-    prettier,
-    {
-        languageOptions: {
-            parserOptions: {
-                project: './tsconfig.json',
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
-        rules: {
-            '@typescript-eslint/no-explicit-any': 'error',
-            '@typescript-eslint/no-unused-vars': [
-                'error',
-                {
-                    argsIgnorePattern: '^_',
-                },
-            ],
-            'functional/no-expression-statements': 'off',
-            'functional/no-return-void': 'off',
-            'functional/functional-parameters': 'off',
-            'functional/no-conditional-statements': 'off',
-        },
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  functional.configs.recommended,
+  functional.configs.externalTypeScriptRecommended,
+  prettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.eslint.json',
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'functional/no-expression-statements': 'off',
+      'functional/no-return-void': 'off',
+      'functional/functional-parameters': 'off',
+      'functional/no-conditional-statements': 'off',
+    },
+  },
 );
